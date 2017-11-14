68408944cc4a7cea40ae87299bba08aa459afa4b
IMAR ALEXANDER AVILA LOPEZ <iavila@ad.elektra.com.mx>
2017-11-14 11:47:11 -0600

    initial commit

---
+++ app/components/promoproduct.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AppActions from '../flux/actions/AppActions.jsx';
+import AppStore from '../flux/stores/AppStore.jsx';
+
+export default class PromoProduct extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	componentDidMount() {
+
+	}
+	render() {
+
+		return (
+			<div id="promoproduct">
+			<h1>Hello!!!</h1>
+			</div>
+		);
+	}
+}
